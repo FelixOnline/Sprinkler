a0ff798b56272182b11a2fbcc122a32fce3d0e1d
Jonathan Kim <jkimbo@gmail.com>
2012-05-24 17:49:04 +0100

    Logging all channels

--- client.js
+++ client.js
@@ -13,22 +13,27 @@ if(!url) {
   process.exit(1);
 }
 
+/*
+ * Add logging to socket by overriding $emit function
+ */
+var addlog = function(socket) {
+  var $emit = socket.$emit;
+  socket.$emit = function() {
+    console.log(socket.name, Array.prototype.slice.call(arguments));
+    $emit.apply(socket, arguments);
+  };
+}
+
 if(channel) { //if a channel is specified
   console.log('Listening to channel '+channel);
   listen += '/'+channel;
+  addlog(io.connect(listen));
 } else { // else listen to all channels
   console.log('No channel, listening to all channels');
   db.channels.on('load', function() { // when db is ready
     db.channels.forEach(function(key, val) { // loop through each channel
-      console.log(key);
+      addlog(io.connect(listen+'/'+key));
     });
   });
 }
 
-var socket = io.connect(listen);
-
-var $emit = socket.$emit;
-socket.$emit = function() {
-  console.log('***','on',Array.prototype.slice.call(arguments));
-  $emit.apply(socket, arguments);
-};
